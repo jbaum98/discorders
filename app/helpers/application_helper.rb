38124dac6ef8eecf915b4d5299a940b285045b67
Jake Waksbaum <jake.waksbaum@gmail.com>
2013-08-27 10:29:39 -0400

    add page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+ def full_title(page_title)
+    base_title = "Disc Orders"
+    if page_title.empty? or page_title.nil?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
