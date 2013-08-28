6493481020f956df888ff52ac83bd04e7f432b7a
Jake Waksbaum <jake.waksbaum@gmail.com>
2013-08-27 23:35:23 -0400

    added signin

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.14'
 gem "bootstrap-sass", "~> 2.3.2.1"
 gem 'bcrypt-ruby', '3.0.1'
 gem 'strong_parameters'
+gem 'turbolinks'
 
 group :development do
   gem 'sqlite3', '1.3.5'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
     	flash[:success] = "#{@user.name} registered!"
       redirect_to root_path
     else
