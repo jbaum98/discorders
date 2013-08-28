963a4ce733f6bc11cb26eaca57bd3f0ed50030c1
Jake Waksbaum <jake.waksbaum@gmail.com>
2013-08-28 08:21:31 -0400

    started to add user_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	before_validation(:name_titleize)
 	before_create :create_remember_token
+  before_create :add_user_id
   validates :name, presence: true, length: {maximum: 50}, uniqueness: true
   has_secure_password
   validates :password, length: { minimum: 6 }
@@ -17,7 +18,11 @@ class User < ActiveRecord::Base
 	def name_titleize
       self.name=name.titleize
   end
+
   private
+
+  def add_user_id
+  end
 
   def database_path
   	"#{Rails.root}/db/#{@user.name.gsub(' ', '_')}"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130828045631) do
+ActiveRecord::Schema.define(:version => 20130828121258) do
 
   create_table "orders", :force => true do |t|
     t.string   "name"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(:version => 20130828045631) do
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.boolean  "received",   :default => false
+    t.integer  "user_id"
   end
+
+  add_index "orders", ["user_id"], :name => "index_orders_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "name"
