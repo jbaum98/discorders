075bf91b7457bb13249ff87683d835003b39a064
Jake Waksbaum <jake.waksbaum@gmail.com>
2013-08-28 00:41:19 -0400

    fancy header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
   	 @user = User.new
   end
-def create
+
+  def create
     @user = User.new(user_params)
     if @user.save
+      create_user_database
       sign_in @user
     	flash[:success] = "#{@user.name} registered!"
       redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   private
