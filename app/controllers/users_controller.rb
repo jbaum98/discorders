29c1f98ee2cca283c1a5a3c7e1a4b095a6766a21
Jake Waksbaum <jake.waksbaum@gmail.com>
2013-08-27 21:21:07 -0400

    flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 def create
     @user = User.new(user_params)
     if @user.save
+    	flash[:success] = "#{@user.name} registered!"
       redirect_to root_path
     else
       render 'new'
